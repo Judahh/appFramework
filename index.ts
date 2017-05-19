877f555db5b806d218654b6836e5a20059a144f3
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-05-19 10:52:06 -0300

    update

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import App from './app';
+import App from './app/app';
 // import './index.css';
 
 ReactDOM.render(React.createElement(App, { name: "Jane" }), document.getElementById('root'));
