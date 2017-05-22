84081100895104aab7c19435cc41d1ccd4540e3d
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-05-22 13:05:22 -0300

    up

--- app/view/common/image/componentImage.ts
+++ app/view/common/image/componentImage.ts
@@ -1,10 +1,10 @@
 importCSS('app/view/common/image/componentImage');
-// importJSON('app/view/jSON/test');
-var test = require('app/view/jSON/test');
+importJSON('app/view/jSON/test');
+// var test = require('app/view/jSON/test');
 
 class ComponentImage{
   render() {
-    
-    return test.mad+Util.elementHTML("image","nasidas2");
+    //  test.mad+
+    return Util.elementHTML("image","nasidas2");
   }
 }
