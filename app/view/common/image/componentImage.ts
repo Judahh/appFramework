ad66e296720368b14644a1f008cb73c56a738b23
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-05-22 15:25:45 -0300

    upda

--- app/view/common/image/componentImage.ts
+++ app/view/common/image/componentImage.ts
@@ -1,16 +1,14 @@
 importCSS('app/view/common/image/componentImage');
-// importJSON('app/view/jSON/test');
-import * as jQuery from "jquery";
 
 class ComponentImage{
   render() {
     //  test.mad+
-    // jQuery.getJSON( "app/view/jSON/test.json", function( data ) {  
-    //     console.log( "JSON Data: " + data);
-    //     jQuery.each( data, function( key, val ) {
-    //         console.log(key + "value:: " + val );
-    //     });
-    // });
+    $.getJSON( "app/view/jSON/test.json", function( data ) {  
+        console.log( "JSON Data: " + data);
+        $.each( data, function( key, val ) {
+            console.log(key + "value:: " + val );
+        });
+    });
     return Util.elementHTML("image","nasidas2");
   }
 }
