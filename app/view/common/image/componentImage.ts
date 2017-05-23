49added13b4415e3f5998247ea9f1bfba77647f1
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-05-23 12:27:02 -0300

    up

--- app/view/common/image/componentImage.ts
+++ app/view/common/image/componentImage.ts
@@ -13,7 +13,7 @@ class ComponentImage{
     Util.getJson("app/view/jSON/test.json").then(
       function(data:ModelImage){
         console.log("JSONT:"+data.source);
-        console.log("FILE:"+this.name);
+        console.log("FILE:"+this.constructor.name);
       }
     ).fail();
     
