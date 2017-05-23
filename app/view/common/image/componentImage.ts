db4b4e02bf7904b03451aa54fd698dd777425f39
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-05-23 12:45:36 -0300

    up

--- app/view/common/image/componentImage.ts
+++ app/view/common/image/componentImage.ts
@@ -3,7 +3,7 @@ importJS('app/view/common/image/modelImage');
 
 importCSS('app/view/common/image/componentImage');
 
-class ComponentImage{
+export class ComponentImage{
   public constructor(){}
 
   public getName() {
@@ -12,14 +12,14 @@ class ComponentImage{
       return (results && results.length > 1) ? results[1] : "";
   }
 
-  render() {
+  public render() {
     //  test.mad+
     // console.log( "JSON Data:");
 
     Util.getJson("app/view/jSON/test.json").then(
       function(data:ModelImage){
         console.log("JSONT:" + data.source);
-        console.log("FILE:" + this.getName);
+        console.log("FILE:" + this.getName());
       }
     ).fail();
     
