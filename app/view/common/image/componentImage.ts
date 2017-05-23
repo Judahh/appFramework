cbaaeb1ccd352c35fa51c5fc4e25dc6161b9b9c0
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-05-23 11:38:07 -0300

    up

--- app/view/common/image/componentImage.ts
+++ app/view/common/image/componentImage.ts
@@ -6,6 +6,12 @@ class ComponentImage{
   render() {
     //  test.mad+
     // console.log( "JSON Data:");
+    $.getJSON('http://hipsterjesus.com/api/').then(function(data) {
+      console.log("JSON:"+data.text);
+    });
+    $.getJSON('app/view/jSON/test.json').then(function(data) {
+      console.log("JSON:"+data.text);
+    });
     Util.getJson("app/view/jSON/test.json").then(
       function(data){
         console.log("JSON:"+data.text);
