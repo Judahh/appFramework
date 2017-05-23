cd94324553b3c886bfab731f22086407f177e2e3
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-05-23 11:54:10 -0300

    up

--- app/view/common/image/componentImage.ts
+++ app/view/common/image/componentImage.ts
@@ -13,21 +13,24 @@ class ComponentImage{
         console.log("JSONT:"+data.text);
         console.log("JSON:"+data);
         $.each( data, function( key, val ) {
-            console.log(key + "value:: " + val );
+            console.log(key + " value:: " + val );
         });
     });
 
-    $.getJSON('app/view/jSON/test.json').done(function(data) {
-      console.log("S:");
-      console.log("JSONT:"+data.text);
-      console.log("JSON:"+data);
-    });
+    // $.getJSON('app/view/jSON/test.json').done(function(data) {
+    //   console.log("S:");
+    //   console.log("JSONT:"+data.text);
+    //   console.log("JSON:"+data);
+    // });
 
     Util.getJson("app/view/jSON/test.json").then(
       function(data){
         console.log("A:");
         console.log("JSONT:"+data.text);
         console.log("JSON:"+data);
+        $.each( data, function( key, val ) {
+            console.log(key + " value:: " + val );
+        });
       }
     ).fail();
     
