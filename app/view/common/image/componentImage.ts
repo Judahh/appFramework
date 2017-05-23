00fc662a8cc0794e4d13971afc9a1689956c7333
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-05-23 12:40:40 -0300

    update

--- app/view/common/image/componentImage.ts
+++ app/view/common/image/componentImage.ts
@@ -6,14 +6,20 @@ importCSS('app/view/common/image/componentImage');
 class ComponentImage{
   public constructor(){}
 
+  public getName() {
+      var funcNameRegex = /function (.{1,})\(/;
+      var results = (funcNameRegex).exec((<any> this).constructor.toString());
+      return (results && results.length > 1) ? results[1] : "";
+  }
+
   render() {
     //  test.mad+
     // console.log( "JSON Data:");
 
     Util.getJson("app/view/jSON/test.json").then(
       function(data:ModelImage){
-        console.log("JSONT:"+data.source);
-        console.log("FILE:" + this.constructor.toString());
+        console.log("JSONT:" + data.source);
+        console.log("FILE:" + this.getName());
       }
     ).fail();
     
