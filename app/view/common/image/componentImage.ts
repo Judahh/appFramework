34b793bb2b21bc7b613011fb1bfc37da9e0e4cb2
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-05-23 10:55:42 -0300

    up

--- app/view/common/image/componentImage.ts
+++ app/view/common/image/componentImage.ts
@@ -8,7 +8,7 @@ class ComponentImage{
     // console.log( "JSON Data:");
     Util.getJson("app/view/jSON/test.json").done(
       function(data){
-        console.log("JSON:"+data.responseText);
+        console.log("JSON:"+data.data);
       }
     ).fail();
     
