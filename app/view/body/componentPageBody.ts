632236b6c6e7fb7d3b75ed8afc54bbc4f7ff7eb0
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-05-23 15:30:14 -0300

    up

--- app/view/body/componentPageBody.ts
+++ app/view/body/componentPageBody.ts
@@ -2,7 +2,7 @@ importJS('app/view/util/util');
 importJS('app/view/common/image/componentImage');
 importJS('app/view/common/component/component');
 
-importCSS('app/view/body/componentBody');
+importCSS('app/view/body/componentPageBody');
 
 class ComponentPageBody extends Component{
   render() {
