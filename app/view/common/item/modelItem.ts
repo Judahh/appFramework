5dbc591a9912fc9c74502d741e08055bc39996ea
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-05-25 10:06:56 -0300

    update

---
+++ app/view/common/item/modelItem.ts
@@ -0,0 +1,7 @@
+importJS('app/view/common/style/modelStyle');
+class ModelItem {
+    routerLink: string;
+    colorEffect: ModelColorEffect;
+    // tooltip:ModelTooltip;
+    // menuVertical:ModelMenuVertical;
+} 
