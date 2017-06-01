41300ae679d8fd87445035218ad1f5ca82b47b2e
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-06-01 16:32:04 -0300

    up

--- app/view/util/util.ts
+++ app/view/util/util.ts
@@ -1,29 +1,40 @@
-class Util{
+class Util {
   static browserLanguage;
   static currentLanguage;
+  static dataJSON: Array<any>;
 
-  static elementHTML(name:string, id?:string, body?:string) {
+  static elementHTML(name: string, id?: string, body?: string) {
     console.log("Name: " + name);
-    var hTML= "<" + name;
-    if(id){
+    var hTML = "<" + name;
+    if (id) {
       hTML += " id=\"" + id;
     }
-    if(body){
-      hTML += "\">"+body+"</"+name;
+    if (body) {
+      hTML += "\">" + body + "</" + name;
     }
-    return hTML+">";
+    return hTML + ">";
   }
 
-  static getJsonPromise(path:string):JQueryPromise<any>{
-    return $.getJSON(path);
+  static getJsonPromise(path: string): JQueryPromise<any> {
+    if (Util.dataJSON == null){
+        Util.dataJSON = new Array();
+    }
+    if (Util.dataJSON[path] == null){
+        Util.dataJSON[path] = $.getJSON(path);
+    }
+    // else{
+    //   console.log("CACHE");
+    // }
+    return Util.dataJSON[path];
+    // return $.getJSON(path);
   }
 
-  static getTag(name:string){
-      var names:string[]=name.split("Component");
-      return names[names.length-1].toLowerCase();
+  static getTag(name: string) {
+    var names: string[] = name.split("Component");
+    return names[names.length - 1].toLowerCase();
   }
 
-  static getFileName(name:string) {
+  static getFileName(name: string) {
     return name.charAt(0).toLowerCase() + name.slice(1);
   }
 
@@ -34,19 +45,19 @@ class Util{
     return link;
   }
 
-  static removeElements(elements:NodeListOf<Element>){
+  static removeElements(elements: NodeListOf<Element>) {
     while (elements[0]) elements[0].parentNode.removeChild(elements[0]);
   }
 
   static getBrowserLanguage() {
-    if(Util.browserLanguage!=undefined){
+    if (Util.browserLanguage != undefined) {
       return Util.browserLanguage;
     }
 
-    var navigator = <any> window.navigator,
-    browserLanguagePropertyKeys = ['language', 'browserLanguage', 'systemLanguage', 'userLanguage'],
-    i,
-    language;
+    var navigator = <any>window.navigator,
+      browserLanguagePropertyKeys = ['language', 'browserLanguage', 'systemLanguage', 'userLanguage'],
+      i,
+      language;
 
     // support for HTML 5.1 "navigator.languages"
     if (Array.isArray(navigator.languages)) {
@@ -72,20 +83,24 @@ class Util{
     return Util.browserLanguage;
   }
 
-  static getCurrentLanguage(){
-    if(Util.currentLanguage!=undefined){
+  static getCurrentLanguage() {
+    if (Util.currentLanguage != undefined) {
       return Util.currentLanguage;
-    }else{
+    } else {
       Util.currentLanguage = Util.getBrowserLanguage();
     }
     return Util.currentLanguage;
   }
 
 }
- 
+
 interface Array<T> {
-    type:any;
-    getType(): string;
+  type: any;
+  getType(): string;
+}
+
+interface JQueryStatic {
+  cache;
 }
 
 // interface Object {
@@ -93,8 +108,8 @@ interface Array<T> {
 //     getConstructor(): any;
 // }
 
-Array.prototype.getType = function() {
-    return this.type;
+Array.prototype.getType = function () {
+  return this.type;
 }
 
 // Object.prototype.getConstructor = function() {
