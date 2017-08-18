363ad8f0d5e47d69816013127a6124b00f4bf744
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2017-08-18 09:10:59 -0300

    update

---
+++ app/view/common/menuHorizontal/centerHolder/componentCenterHolder.ts
@@ -0,0 +1,15 @@
+importJS('app/view/util/util');
+importJS('app/view/serviceModel/serviceModel');
+importJS('app/view/common/component/component');
+importJS('app/view/common/item/componentItem');
+
+class ComponentCenterHolder extends Component{
+  class: string;
+  arrayItem: Array<ComponentItem>;
+
+  constructor(father?: Component){
+    super(father);
+    this.arrayItem = new Array<ComponentItem>();
+    this.arrayItem.type=ComponentItem;
+  }
+}
--- app/view/common/menuHorizontal/componentMenuHorizontal.ts
+++ app/view/common/menuHorizontal/componentMenuHorizontal.ts
@@ -1,15 +1,20 @@
 importJS('app/view/util/util');
 importJS('app/view/serviceModel/serviceModel');
 importJS('app/view/common/component/component');
-importJS('app/view/common/item/componentItem');
+importJS('app/view/common/menuHorizontal/centerHolder/componentCenterHolder');
+importJS('app/view/common/menuHorizontal/leftHolder/componentLeftHolder');
+importJS('app/view/common/menuHorizontal/rightHolder/componentRightHolder');
 
 class ComponentMenuHorizontal extends Component{
   class: string;
-  arrayItem: Array<ComponentItem>;
+  centerHolder: ComponentCenterHolder;
+  leftHolder: ComponentLeftHolder;
+  rightHolder: ComponentRightHolder;
 
   constructor(father?: Component){
     super(father);
-    this.arrayItem = new Array<ComponentItem>();
-    this.arrayItem.type=ComponentItem;
+    this.leftHolder = new ComponentLeftHolder(this);
+    this.centerHolder = new ComponentCenterHolder(this);
+    this.rightHolder = new ComponentRightHolder(this);
   }
 }
