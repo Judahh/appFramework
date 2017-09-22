fff1ce6663b049d53da7764f18d4492480ccf157
Judahh <judahholanda7@gmail.com>
2017-09-22 09:49:43 -0300

    update

--- app/view/common/dataInput/componentDataInput.ts
+++ app/view/common/dataInput/componentDataInput.ts
@@ -3,7 +3,7 @@ importJS('app/view/common/component/component');
 importJS('app/view/common/textInput/typeTextInput');
 importJS('app/view/common/textInput/option/componentOption');
 
-class ComponentDataInput extends Component {
+class ComponentDataInput extends ComponentItem {
   //IF DATALIST IT NEEDS A INPUT
   //<input list="datalistID" name="inputNAME">
   //<datalist id="datalistID">
@@ -12,6 +12,7 @@ class ComponentDataInput extends Component {
   constructor(father?: Component, type?:TypeDataInput) {
     super(father, ComponentDataInput.typeTag(type));
     this.type = type;
+    console.log("a");
   }
 
   public static typeTag(type:TypeDataInput){
