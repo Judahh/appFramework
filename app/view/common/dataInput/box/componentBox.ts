29770a81c922643501bdf3ab6b85b6f06e1175ce
Judahh <judahholanda7@gmail.com>
2017-10-10 15:50:51 -0300

    update

--- app/view/common/dataInput/box/componentBox.ts
+++ app/view/common/dataInput/box/componentBox.ts
@@ -1,11 +1,15 @@
 importJS('app/view/util/util');
 importJS('app/view/common/component/component');
-importJS('app/view/common/dataInput/comboBox/option/componentOption');
+importJS('app/view/common/label/componentLabel');
 
 class ComponentBox extends Component {
   //type = radio or checkbox
+  boxLabel: ComponentLabel;
 
   constructor(father?: Component, tag?) {
     super(father, "input");
+    this.boxLabel = new ComponentLabel(this.father);
+    this.boxLabel.getElement().setAttribute("for", this.element.id);
   }
+
 }
---
+++ app/view/common/label/componentLabel.ts
@@ -0,0 +1,7 @@
+importJS('app/view/util/util');
+importJS('app/view/common/component/component');
+class ComponentLabel extends Component {
+    constructor(father?: Component, tag?) {
+        super(father, "label");
+    }
+}
