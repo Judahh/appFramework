a07c1ca7398bf7a7f4dd3b450f4510ac6e64fd91
Judahh <judahholanda7@gmail.com>
2017-11-14 08:43:24 -0300

    update

--- app/view/common/dataInput/keyboard/componentKeyboard.ts
+++ app/view/common/dataInput/keyboard/componentKeyboard.ts
@@ -1,7 +1,7 @@
 import { Component } from './../../component/component';
-// import * as $ from 'jquery';
+import * as $ from 'jquery';
 // import { Util } from './../../../util/util';
-// import { KeyboardOptions, Typing, Extender } from 'virtual-keyboard';
+import { KeyboardOptions, Typing, Extender } from 'virtual-keyboard';
 try { require('./componentKeyboard.css'); } catch (e) { };
 
 export class ComponentKeyboard extends Component {
@@ -11,22 +11,18 @@ export class ComponentKeyboard extends Component {
 
   constructor(father?: Component, tag?) {
     super(father, 'virtualKeyboard');
-    // let keyboardOptionsB = KeyboardOptions;
+    let keyboardOptionsB = KeyboardOptions;
     // let typingB = Typing;
     // let extenderB = Extender;
   }
 
   public renderAfterUpdateJSON() {
     let _self = this;
-    let element;// = <any>$('#' + this.element.id);
+    let element = <any>$('#' + this.father.getElement().id);
     // let jQueryB = $;
     // eval('$ = jQueryB');
-    let id='#'+this.element.id;
-    console.log('ID:'+id)
-    console.log('_self.keyboardOptions:',_self.keyboardOptions)
-    console.log('_self.typing:',_self.typing)
-    console.log('_self.extender:',_self.extender)
-    eval('element = $(id);');
+    // let id = '#' + this.father.getElement().id;
+    // eval('element = $(id);');
     element.keyboard(_self.keyboardOptions);
     // .addTyping(_self.typing)
     // .addExtender(_self.extender);
