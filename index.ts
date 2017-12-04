74f9d1ca54cccfbb8ede4e5f6ef0929ea0686fef
Judahh <judahholanda7@gmail.com>
2017-12-04 13:09:53 -0300

    update

--- index.ts
+++ index.ts
@@ -9,30 +9,30 @@ import { ComponentVideo } from './app/view/common/videoHolder/video/componentVid
 import { ComponentOption } from './app/view/common/dataInput/comboBox/option/componentOption';
 import { ComponentComboBox } from './app/view/common/dataInput/comboBox/componentComboBox';
 import { ComponentTextArea } from './app/view/common/dataInput/textArea/componentTextArea';
-import { ComponentDataList} from './app/view/common/dataInput/textField/dataList/componentDataList'
-import { ComponentTextField } from './app/view/common/dataInput/textField/componentTextField'
-import { ComponentButton } from './app/view/common/dataInput/button/componentButton'
-import { ComponentBox } from './app/view/common/dataInput/box/componentBox'
-import { ComponentRangeSlider } from './app/view/common/dataInput/rangeSlider/componentRangeSlider'
-import { ComponentElementEvent } from './app/view/common/elementEvent/componentElementEvent'
-import { ComponentForm } from './app/view/common/form/componentForm'
-import { ComponentImg } from './app/view/common/image/img/componentImg'
-import { ComponentImage } from './app/view/common/image/componentImage'
-import { ComponentAnimationSubEffectHolder } from './app/view/common/item/colorEffect/font/animationEffect/animationSubEffect/animationSubEffectHolder/componentAnimationSubEffectHolder'
-import { ComponentAnimationSubEffect } from './app/view/common/item/colorEffect/font/animationEffect/animationSubEffect/componentAnimationSubEffect'
-import { ComponentAnimationEffect } from './app/view/common/item/colorEffect/font/animationEffect/componentAnimationEffect'
-import { ComponentFont } from './app/view/common/item/colorEffect/font/componentFont'
-import { ComponentColorEffect } from './app/view/common/item/colorEffect/componentColorEffect'
-import { ComponentItem } from './app/view/common/item/componentItem'
-import { ComponentLabel } from './app/view/common/label/componentLabel'
-import { ComponentMap } from './app/view/common/map/componentMap'
-import { ComponentCenterHolder } from './app/view/common/menuHorizontal/centerHolder/componentCenterHolder'
-import { ComponentLeftHolder } from './app/view/common/menuHorizontal/leftHolder/componentLeftHolder'
-import { ComponentRightHolder } from './app/view/common/menuHorizontal/rightHolder/componentRightHolder'
-import { ComponentMenuHorizontal } from './app/view/common/menuHorizontal/componentMenuHorizontal'
-import { ComponentMenuVertical } from './app/view/common/menuVertical/componentMenuVertical'
-import { ComponentProgressBar } from './app/view/common/progressBar/componentProgressBar'
-import { ComponentSource } from './app/view/common/source/componentSource'
+import { ComponentDataList} from './app/view/common/dataInput/textField/dataList/componentDataList';
+import { ComponentTextField } from './app/view/common/dataInput/textField/componentTextField';
+import { ComponentButton } from './app/view/common/dataInput/button/componentButton';
+import { ComponentBox } from './app/view/common/dataInput/box/componentBox';
+import { ComponentRangeSlider } from './app/view/common/dataInput/rangeSlider/componentRangeSlider';
+import { ComponentElementEvent } from './app/view/common/elementEvent/componentElementEvent';
+import { ComponentForm } from './app/view/common/form/componentForm';
+import { ComponentImg } from './app/view/common/image/img/componentImg';
+import { ComponentImage } from './app/view/common/image/componentImage';
+import { ComponentAnimationSubEffectHolder } from './app/view/common/item/colorEffect/font/animationEffect/animationSubEffect/animationSubEffectHolder/componentAnimationSubEffectHolder';
+import { ComponentAnimationSubEffect } from './app/view/common/item/colorEffect/font/animationEffect/animationSubEffect/componentAnimationSubEffect';
+import { ComponentAnimationEffect } from './app/view/common/item/colorEffect/font/animationEffect/componentAnimationEffect';
+import { ComponentFont } from './app/view/common/item/colorEffect/font/componentFont';
+import { ComponentColorEffect } from './app/view/common/item/colorEffect/componentColorEffect';
+import { ComponentItem } from './app/view/common/item/componentItem';
+import { ComponentLabel } from './app/view/common/label/componentLabel';
+import { ComponentMap } from './app/view/common/map/componentMap';
+import { ComponentCenterHolder } from './app/view/common/menuHorizontal/centerHolder/componentCenterHolder';
+import { ComponentLeftHolder } from './app/view/common/menuHorizontal/leftHolder/componentLeftHolder';
+import { ComponentRightHolder } from './app/view/common/menuHorizontal/rightHolder/componentRightHolder';
+import { ComponentMenuHorizontal } from './app/view/common/menuHorizontal/componentMenuHorizontal';
+import { ComponentMenuVertical } from './app/view/common/menuVertical/componentMenuVertical';
+import { ComponentProgressBar } from './app/view/common/progressBar/componentProgressBar';
+import { ComponentSource } from './app/view/common/source/componentSource';
 import { ComponentCircle } from './app/view/common/sVG/circle/componentCircle';
 import { ComponentEllipse } from './app/view/common/sVG/ellipse/componentEllipse';
 import { ComponentRectangle } from './app/view/common/sVG/rectangle/componentRectangle';
@@ -42,25 +42,25 @@ import { ComponentLine } from './app/view/common/sVG/line/componentLine';
 import { ComponentPath } from './app/view/common/sVG/path/componentPath';
 import { ComponentText } from './app/view/common/sVG/text/componentText';
 import { ComponentG } from './app/view/common/sVG/g/componentG';
-import { ComponentFilter } from './app/view/common/sVG/defs/filter/componentFilter'
-import { ComponentLinearGradient } from './app/view/common/sVG/defs/linearGradient/componentLinearGradient'
-import { ComponentRadialGradient } from './app/view/common/sVG/defs/radialGradient/componentRadialGradient'
-import { ComponentStop } from './app/view/common/sVG/defs/stop/componentStop'
-import { ComponentDefs } from './app/view/common/sVG/defs/componentDefs'
-import { ComponentFooter } from './app/view/footer/componentFooter'
-import { ComponentHeader } from './app/view/header/componentHeader'
-import { ServiceModel } from './app/view/serviceModel/serviceModel'
-import { Util } from './app/view/util/util'
-import { ComponentView } from './app/view/componentView'
-import { App } from './app/app'
-import { ImportScript } from './importScript'
+import { ComponentFilter } from './app/view/common/sVG/defs/filter/componentFilter';
+import { ComponentLinearGradient } from './app/view/common/sVG/defs/linearGradient/componentLinearGradient';
+import { ComponentRadialGradient } from './app/view/common/sVG/defs/radialGradient/componentRadialGradient';
+import { ComponentStop } from './app/view/common/sVG/defs/stop/componentStop';
+import { ComponentDefs } from './app/view/common/sVG/defs/componentDefs';
+import { ComponentFooter } from './app/view/footer/componentFooter';
+import { ComponentHeader } from './app/view/header/componentHeader';
+import { ServiceModel } from './app/view/serviceModel/serviceModel';
+import { Util } from './app/view/util/util';
+import { ComponentView } from './app/view/componentView';
+import { App } from './app/app';
+import { ImportScript } from './importScript';
 import { AppObject } from './app/view/common/appObject/appObject';
-import { ComponentChart } from './app/view/common/chart/componentChart'
-import { AppObjectFactory } from './app/view/common/appObject/appObjectFactory/appObjectFactory'
-import { ComponentBackground } from './app/view/body/background/componentBackground'
-import { ApiConnection } from './app/view/apiConnection/apiConnection'
-import { Observer } from './app/observer/observer'
-
+import { ComponentChart } from './app/view/common/chart/componentChart';
+import { AppObjectFactory } from './app/view/common/appObject/appObjectFactory/appObjectFactory';
+import { ComponentBackground } from './app/view/body/background/componentBackground';
+import { ApiConnection } from './app/view/apiConnection/apiConnection';
+import { Observer } from './app/observer/observer';
+import { Socket } from './app/socket/socket';
 
 export { Component, ComponentPageBody, ComponentInformation, ComponentDataInput, ComponentOption,
     ComponentDivisor, ComponentVideoHolder, ComponentIframe, ComponentVideo, ComponentComboBox,
@@ -72,5 +72,5 @@ export { Component, ComponentPageBody, ComponentInformation, ComponentDataInput,
     ComponentSource, ComponentCircle, ComponentEllipse, ComponentRectangle, ComponentPolygon, ComponentLine,
     ComponentPath, ComponentText, ComponentG, ComponentFilter, ComponentLinearGradient, ComponentRadialGradient,
     ComponentStop, ComponentDefs, ComponentFooter, ComponentHeader, ServiceModel, Util, ComponentView, App, AppObject, ImportScript,
-    ComponentChart, AppObjectFactory, ComponentBackground, ApiConnection, Observer
+    ComponentChart, AppObjectFactory, ComponentBackground, ApiConnection, Observer, Socket
 }
