09a32ae9b9eebf046cced29f74e62403f8e8c013
Judah Holanda <judah.holanda@lds.ifce.edu.br>
2019-09-13 08:42:36 -0300

    update

--- app/view/common/item/information/componentBasicInformation.ts
+++ app/view/common/item/information/componentBasicInformation.ts
@@ -12,7 +12,7 @@ export class ComponentBasicInformation extends ComponentValue {
     super(tag, father, 'text');
     let _self = this;
     _self.className = 'ComponentBasicInformation';
-    ko.applyBindings(new BasicViewModel('text', _self.element, ko)); // receive BasicModel in cosntructo and save
+    ko.applyBindings(new BasicViewModel('text', _self.element, ko), _self.element); // receive BasicModel in cosntructo and save
   }
 
   public renderAfterUpdate() {
